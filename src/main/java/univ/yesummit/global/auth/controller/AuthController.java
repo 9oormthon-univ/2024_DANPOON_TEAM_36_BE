2d403ba1f502194411db6cb8e8167d9dfcf11d42
sonsumin <sonsumin00@gmail.com>
2024-11-19 23:01:09 +0900

    [#3]✨Feat: 소셜 로그인 엔드포인트 설정

---
+++ src/main/java/univ/yesummit/global/auth/controller/AuthController.java
@@ -0,0 +1,16 @@
+package univ.yesummit.global.auth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+@RestController
+@RequestMapping("/v1/api/kakao")
+public class AuthController {
+
+    @GetMapping("/login")
+    public RedirectView login() {
+        return new RedirectView("/oauth2/authorization/kakao");
+    }
+}
