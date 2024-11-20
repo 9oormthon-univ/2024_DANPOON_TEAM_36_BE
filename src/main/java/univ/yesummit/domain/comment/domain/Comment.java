5b077fe4f46d0f6206301a8ac60f91d295390564
Lee, yeeun <ye7426@naver.com>
2024-11-21 06:16:41 +0900

    :sparkles:Feat: PT 영상 게시글 댓글 관련 엔터티 작성

---
+++ src/main/java/univ/yesummit/domain/comment/domain/Comment.java
@@ -0,0 +1,55 @@
+package univ.yesummit.domain.comment.domain;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import univ.yesummit.domain.board.domain.Board;
+import univ.yesummit.domain.member.entity.Member;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "comment_id")
+    private Long commentId;
+
+    @Column(columnDefinition = "TEXT")
+    @Schema(description = "댓글", example = "댓글")
+    private String comment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member writer;
+
+    @Builder
+    private Comment(String comment, Member writer, Board board) {
+        this.comment = comment;
+        this.writer = writer;
+        this.board = board;
+    }
+
+    public void updateComment(String comment) {
+
+        if (comment == null || comment.trim().isEmpty()) {
+            throw new IllegalArgumentException("댓글 내용은 비어 있을 수 없습니다.");
+        }
+        if (this.comment.equals(comment)) {
+            // 새로운 내용이 현재 내용과 동일한 경우, 아무 작업도 하지 않음
+            return;
+        }
+        this.comment = comment;
+
+        System.out.println("댓글이 수정되었습니다: " + comment);
+    }
+
+}
