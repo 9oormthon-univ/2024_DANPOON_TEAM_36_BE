fce60c5af2915b4b4f03ec21bea44b2af82a8128
sonsumin <sonsumin00@gmail.com>
2024-11-23 00:43:36 +0900

    [#36]✨Feat: 써밋 레포 생성

--- src/main/java/univ/yesummit/domain/summit/repository/SummitRepository.java
+++ src/main/java/univ/yesummit/domain/summit/repository/SummitRepository.java
@@ -1,7 +1,7 @@
-package univ.yesummit.domain.gpt.repository;
+package univ.yesummit.domain.summit.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import univ.yesummit.domain.gpt.entity.Summit;
+import univ.yesummit.domain.summit.entity.Summit;
 
 public interface SummitRepository extends JpaRepository<Summit, Long> {
 }
