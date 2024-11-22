92789bee836533537946c3a452693cb9b99de381
sonsumin <sonsumin00@gmail.com>
2024-11-23 00:41:38 +0900

    [#36]✨Feat: 써밋 엔티티 생성

---
+++ src/main/java/univ/yesummit/domain/gpt/entity/Summit.java
@@ -0,0 +1,27 @@
+package univ.yesummit.domain.gpt.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Summit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String content;
+
+    public Summit(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
