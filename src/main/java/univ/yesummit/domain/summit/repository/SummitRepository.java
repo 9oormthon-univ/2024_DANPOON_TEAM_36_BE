3ccf971de123eaa12da552cd51c03746e4845713
sonsumin <sonsumin00@gmail.com>
2024-11-23 00:56:48 +0900

    [#36]✨Feat: 써밋 레포지토리 작성

--- src/main/java/univ/yesummit/domain/summit/repository/SummitRepository.java
+++ src/main/java/univ/yesummit/domain/summit/repository/SummitRepository.java
@@ -3,5 +3,9 @@ package univ.yesummit.domain.summit.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import univ.yesummit.domain.summit.entity.Summit;
 
+import java.util.Optional;
+
 public interface SummitRepository extends JpaRepository<Summit, Long> {
+
+    Optional<Summit> findTopByOrderByIdDesc();
 }
