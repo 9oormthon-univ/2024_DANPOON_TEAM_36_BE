193af1b475f5f0025c2465db234a2bf3cce63d5a
sonsumin <sonsumin00@gmail.com>
2024-11-23 21:18:33 +0900

    [#2]✨Feat: 토큰 발급 시 리프레시 토큰 저장

--- src/main/java/univ/yesummit/global/auth/service/impl/AuthServiceImpl.java
+++ src/main/java/univ/yesummit/global/auth/service/impl/AuthServiceImpl.java
@@ -2,9 +2,13 @@ package univ.yesummit.global.auth.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import univ.yesummit.domain.member.entity.Member;
+import univ.yesummit.domain.member.exception.MemberException;
+import univ.yesummit.domain.member.repository.MemberRepository;
 import univ.yesummit.domain.member.service.MemberService;
 import univ.yesummit.global.auth.service.AuthService;
 import univ.yesummit.global.auth.util.JwtUtils;
+import univ.yesummit.global.exception.ErrorCode;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,11 +20,19 @@ public class AuthServiceImpl implements AuthService {
 
     private final JwtUtils jwtUtils;
     private final MemberService memberService;
+    private final MemberRepository memberRepository;
 
     public Map<String, Object> generateTokens(Long userId) {
+
+        Member member = memberRepository.findById(userId)
+                .orElseThrow(() -> new MemberException(ErrorCode.NOT_FOUND_MEMBER));
+
+
         // JWT 토큰 생성
         String accessToken = jwtUtils.createAccessToken(userId);
         String refreshToken = jwtUtils.createRefreshToken(userId);
+
+        member.updateRefreshToken(refreshToken);
 
         // 첫 로그인 여부 확인
         boolean isFirstLogin = memberService.isFirstLogin(userId);
